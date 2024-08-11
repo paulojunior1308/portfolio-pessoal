100493a043d4c6e4137b32bd7f83c9f88c26293e
paulo.junior <pauloesjr2@gmail.com>
2024-08-11 20:55:43 -0300

    Portfolio

---
+++ Backend/src/test/java/portifolio/CurriculoOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package portifolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculoOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
