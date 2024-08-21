6cc4303d0213297e929ed1687e1f35e740a1cf4d
paulojunior1308 <pauloesjr2@gmail.com>
2024-08-21 14:40:20 -0300

    Site

--- Frontend/portfolio-pessoal/src/js/Sobre.js
+++ Frontend/portfolio-pessoal/src/js/Sobre.js
@@ -1,16 +1,18 @@
 import React, { useEffect } from "react";
-import AOS from 'aos'; // Importando AOS
-import 'aos/dist/aos.css'; // Importando os estilos do AOS
+import AOS from "aos"; // Importando AOS
+import "aos/dist/aos.css"; // Importando os estilos do AOS
 import "../css/responsividade.css";
 import "../css/App.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import ToggleMenu from "./ToggleMenu";
-import AboutImage from "../image/about-img.png";
+import AboutImage from "../image/about-img.jpeg";
 import Css from "../image/css.jpg";
 import Java from "../image/java.jpg";
 import ReactJs from "../image/react.jpg";
 import Spring from "../image/springboot.jpg";
 import Javascript from "../image/javascript.jpg";
+import Sql from "../image/sql.jpeg";
+import Api from "../image/api.jpeg";
 import Curriculo from "../image/arquivos/CV-PauloJunior.pdf";
 
 const Sobre = () => {
@@ -33,8 +35,24 @@ const Sobre = () => {
           <div className="content-about">
             <div className="content-about-left" data-aos="fade-right">
               <h1>Olá, me chamo Paulo Junior</h1>
-              <p>Sou formado em Ciências da Computação com uma pós-graduação em Segurança da Informação. Atualmente, estou em um processo de transição para a área de desenvolvimento de software, focando principalmente em Java e Spring Boot. Tenho experiência acadêmica sólida e estou ampliando meus conhecimentos práticos, estudando também React e CSS para criar aplicações web modernas e interativas.</p>
-              <p>Estou desenvolvendo este portfólio para demonstrar minhas habilidades e projetos, enquanto busco oportunidades para ingressar no mercado de trabalho como desenvolvedor. Estou motivado a aplicar minha paixão por tecnologia e meu compromisso com a aprendizagem contínua para criar soluções eficientes e seguras. Meu objetivo é crescer na área de desenvolvimento, contribuindo com projetos desafiadores e inovadores.</p>
+              <p>
+                Sou formado em Ciências da Computação com uma pós-graduação em
+                Segurança da Informação. Atualmente, estou em um processo de
+                transição para a área de desenvolvimento de software, focando
+                principalmente em Java e Spring Boot. Tenho experiência
+                acadêmica sólida e estou ampliando meus conhecimentos práticos,
+                estudando também React e CSS para criar aplicações web modernas
+                e interativas.
+              </p>
+              <p>
+                Estou desenvolvendo este portfólio para demonstrar minhas
+                habilidades e projetos, enquanto busco oportunidades para
+                ingressar no mercado de trabalho como desenvolvedor. Estou
+                motivado a aplicar minha paixão por tecnologia e meu compromisso
+                com a aprendizagem contínua para criar soluções eficientes e
+                seguras. Meu objetivo é crescer na área de desenvolvimento,
+                contribuindo com projetos desafiadores e inovadores.
+              </p>
               <div className="cta-about">
                 <div className="btn-about" data-aos="fade-down">
                   <a href={Curriculo} download="CV-PauloJunior.pdf">
@@ -78,22 +96,53 @@ const Sobre = () => {
           <div className="skills-headline" data-aos="fade-down">
             <span className="destaque">Conheça um pouco minhas</span>
             <h1>Habilidades</h1>
-            <p>Minha formação acadêmica é voltada para a Segurança da Informação, onde adquiri conhecimentos sólidos em criptografia, legislação, normas de segurança e identificação de vulnerabilidades. Agora, estou direcionando meus esforços para a área de desenvolvimento de software, especialmente em Java e Spring Boot, com o objetivo de expandir minhas habilidades e me tornar um desenvolvedor versátil. Embora meu conhecimento em desenvolvimento ainda esteja em crescimento, estou empenhado em aprender e aplicar práticas de programação modernas. Minha experiência prévia em suporte técnico, junto com a forte base em segurança, me fornece uma perspectiva única para criar soluções de software que são tanto funcionais quanto seguras.</p>
+            <p>
+              Minha formação acadêmica é voltada para a Segurança da Informação,
+              onde adquiri conhecimentos sólidos em criptografia, legislação,
+              normas de segurança e identificação de vulnerabilidades. Agora,
+              estou direcionando meus esforços para a área de desenvolvimento de
+              software, especialmente em Java e Spring Boot, com o objetivo de
+              expandir minhas habilidades e me tornar um desenvolvedor versátil.
+              Embora meu conhecimento em desenvolvimento ainda esteja em
+              crescimento, estou empenhado em aprender e aplicar práticas de
+              programação modernas. Minha experiência prévia em suporte técnico,
+              junto com a forte base em segurança, me fornece uma perspectiva
+              única para criar soluções de software que são tanto funcionais
+              quanto seguras.
+            </p>
             <div className="list-about" data-aos="fade-up">
               <span className="destaque">Soft skills</span>
               <div className="container-list">
                 <ul>
-                  <li><i className="fa-solid fa-angles-right"></i> Comunicação</li>
-                  <li><i className="fa-solid fa-angles-right"></i> Colaboração</li>
-                  <li><i className="fa-solid fa-angles-right"></i> Resolução</li>
-                  <li><i className="fa-solid fa-angles-right"></i> Adaptabilidade</li>
-                  <li><i className="fa-solid fa-angles-right"></i> Pontualidade</li>
+                  <li>
+                    <i className="fa-solid fa-angles-right"></i> Comunicação
+                  </li>
+                  <li>
+                    <i className="fa-solid fa-angles-right"></i> Colaboração
+                  </li>
+                  <li>
+                    <i className="fa-solid fa-angles-right"></i> Resolução
+                  </li>
+                  <li>
+                    <i className="fa-solid fa-angles-right"></i> Adaptabilidade
+                  </li>
+                  <li>
+                    <i className="fa-solid fa-angles-right"></i> Pontualidade
+                  </li>
                 </ul>
                 <ul>
-                  <li><i className="fa-solid fa-angles-right"></i> Detalhismo</li>
-                  <li><i className="fa-solid fa-angles-right"></i> Crítica</li>
-                  <li><i className="fa-solid fa-angles-right"></i> Criatividade</li>
-                  <li><i className="fa-solid fa-angles-right"></i> Autodidatismo</li>
+                  <li>
+                    <i className="fa-solid fa-angles-right"></i> Detalhismo
+                  </li>
+                  <li>
+                    <i className="fa-solid fa-angles-right"></i> Crítica
+                  </li>
+                  <li>
+                    <i className="fa-solid fa-angles-right"></i> Criatividade
+                  </li>
+                  <li>
+                    <i className="fa-solid fa-angles-right"></i> Autodidatismo
+                  </li>
                 </ul>
               </div>
             </div>
@@ -119,11 +168,23 @@ const Sobre = () => {
               <img src={Spring} alt="Springboot" />
               <p>Springboot</p>
             </div>
+            <div className="card-tech" data-aos="zoom-in">
+              <img src={Sql} alt="Sql" />
+              <p>SQL</p>
+            </div>
+            <div className="card-tech" data-aos="zoom-in">
+              <img src={Api} alt="Api" />
+              <p>Rest API</p>
+            </div>
           </div>
         </div>
       </section>
       <footer className="sobre-footer">
-        <p>Todos os direitos reservados<br />Desenvolvido <span className="sobre-destaque">Paulo Junior│Dev.</span></p>
+        <p>
+          Todos os direitos reservados
+          <br />
+          Desenvolvido <span className="sobre-destaque">Paulo Junior│Dev.</span>
+        </p>
       </footer>
     </main>
   );
