71402dd296a266b776e86618e2435fb75bb25f65
paulojunior1308 <pauloesjr2@gmail.com>
2024-08-25 18:02:56 -0300

    Estoque

--- Backend/estoque/src/main/java/projeto/estoque/EstoqueApplication.java
+++ Backend/estoque/src/main/java/projeto/estoque/EstoqueApplication.java
@@ -1,14 +1,13 @@
-package portfolio;
+package projeto.estoque;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CurriculoOnlineApplication {
+public class EstoqueApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CurriculoOnlineApplication.class, args);
-
+		SpringApplication.run(EstoqueApplication.class, args);
 	}
 
 }
--- Backend/estoque/src/test/java/projeto/estoque/EstoqueApplicationTests.java
+++ Backend/estoque/src/test/java/projeto/estoque/EstoqueApplicationTests.java
@@ -1,10 +1,10 @@
-package portifolio;
+package projeto.estoque;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CurriculoOnlineApplicationTests {
+class EstoqueApplicationTests {
 
 	@Test
 	void contextLoads() {
